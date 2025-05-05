b9e38a3a42344b02ef4d6cda073aaa05bb747edf
ninaahuangg <thenina207@gmail.com>
2025-05-05 03:18:32 +0000

    1

---
+++ Main.java
@@ -0,0 +1,18 @@
+
+public class Main 
+{
+    public static void main(String[] args)
+    {
+        FrogSimulation sim = new FrogSimulation(24,5);
+        System.out.println(sim.simulate());
+        FrogSimulation simTwo = new FrogSimulation(25,4);
+        System.out.println(simTwo.simulate());
+        FrogSimulation simThree = new FrogSimulation(31,3);
+        System.out.println(simThree.simulate());
+        FrogSimulation simFour = new FrogSimulation(-2,3);
+        System.out.println(simFour.simulate());
+        FrogSimulation simFive = new FrogSimulation(18,3);
+        System.out.println(simFive.simulate());
+        System.out.println(sim.runSimulations(4));
+    }
+}
